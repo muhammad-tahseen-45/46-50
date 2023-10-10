1d20fde88c89c241f346952df866182462a97a7f
muhammad-tahseen-45 <144385363+muhammad-tahseen-45@users.noreply.github.com>
2023-10-10 18:21:47 +0500

    Add files via upload

---
+++ ch46to50.js
@@ -0,0 +1,125 @@
+function grt(){
+
+alert("hello")
+
+
+
+}
+
+function grt1(){
+
+   document.getElementById("tt").style.display="none"
+    
+    
+    }
+
+
+    
+function chcol(x){
+
+  
+    x.style = 'background-color: yellow';
+
+
+     }
+
+
+     function rr(x){
+
+  
+        x.style = 'background-color: red';
+    
+    
+         }
+    
+
+         function rr1(x){
+
+  
+
+         x.onclick = () => window.scrollTo({ top: 0, behavior: 'smooth' })
+
+
+         }
+
+         function rr11(x){
+
+         window.open("");
+
+         }
+
+         function rr111(x){
+alert("hi helo")
+            }
+
+
+            
+         function rr1112(x){
+         
+        x.style = 'background-color: powderblue';
+    
+    
+
+                        }
+
+
+                        function rr11121(x){
+         
+                         alert("hi")
+                        
+                    
+                                            }
+
+
+
+                                            
+function rr11121h(){
+
+    document.getElementById("p123").style.display="none"
+     
+     
+     }
+
+
+
+
+     function pt1(y){
+
+        var y=document.getElementById("t1").value;
+
+        console.log(y)
+         
+         
+         }
+    
+     
+
+         function pt1t(y){
+
+           document.getElementById("t1").value="sahir";
+    
+            console.log(y)
+             
+         }  
+ 
+
+         function pt12(y){
+
+            var y=document.getElementById("ch1").value;
+    
+            console.log(y)
+             
+             
+             }
+
+
+
+             
+         function pt12s(y){
+
+            var y=document.getElementById("books").value;
+    
+            console.log(y)
+             
+             
+             }
